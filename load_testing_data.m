c3cdeef089eeaaeaa7d25c3f9480af575a451c3d
Vineet R. Shenoy <vineetrshenoy@gmail.com>
2020-09-24 14:34:02 -0400

    Fixed data loading problem

--- load_testing_data.m
+++ load_testing_data.m
@@ -3,12 +3,18 @@ function [train_mat] = get_testing_data(direc)
     train_mat = zeros(112*92, 40*1);
     D = dir(direc);
     count = 1;
-    for i=4:length(D)
+
+    D1 = string(["s1","s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10"]);
+    D2 = string(["s11","s12", "s13", "s14", "s15", "s16", "s17", "s18", "s19", "s20"]);
+    D3 = string(["s21","s22", "s23", "s24", "s25", "s26", "s27", "s28", "s29", "s30"]);
+    D4 = string(["s31","s32", "s33", "s34", "s35", "s36", "s37", "s38", "s39", "s40"]);
+    D = [D1, D2, D3, D4];
+    for i=1:length(D)
         
-        files = dir(fullfile(direc, D(i).name));
+        files = dir(fullfile(direc, D(i)));
         
         
-        A = imread(fullfile(direc, D(i).name, files(3).name));
+        A = imread(fullfile(direc, D(i), files(3).name));
         B = reshape(A, [], 1);
         train_mat(:, count) = B(:);
         count = count + 1;
